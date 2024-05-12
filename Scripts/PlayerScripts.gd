db3aba308d2ec09b98d1bc3f7ee537b7d434b259
Pe4enka <Frakia2630@mail.ru>
2024-05-13 02:53:10 +0300

    Upload_6

--- Scripts/PlayerScripts.gd
+++ Scripts/PlayerScripts.gd
@@ -1,3 +1,15 @@
 extends CharacterBody2D
 
-@export var speed = 150
+@export var Max_health = 100
+@onready var Health = 65
+var speed = 150
+
+func _ready():
+	set_HP(Health, Max_health)
+
+func set_HP(Health, Max_health):
+	$BarHP.value = Health
+	$BarHP.max_value = Max_health	
+
+func update_health():
+	$BarHP.value = Health	
